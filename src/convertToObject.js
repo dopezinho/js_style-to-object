20dc3b87df1d2c6b6b74cdcd28037925b6c67b22
dopezinho <opugnero@gmail.com>
2025-06-25 12:39:36 -0300

    fixed declaration

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -16,9 +16,16 @@ function convertToObject(sourceString) {
       continue;
     }
 
-    const [key, value] = ar.split(':');
+    const index = ar.indexOf(':');
 
-    result[key.trim()] = value.trim();
+    if (index === -1) {
+      continue;
+    }
+
+    const key = ar.slice(0, index).trim();
+    const value = ar.slice(index + 1).trim();
+
+    result[key] = value;
   }
 
   return result;
